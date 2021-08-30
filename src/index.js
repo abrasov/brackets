3213b0f63ab83cd1e5b7acff7e189ed0d2455e60
abrasov <abrasovs@yandex.ru>
2021-08-31 00:19:00 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const newBrackets = bracketsConfig.map((el) => el.join(''));
+  for (let i = 0; i < newBrackets.length; i++) {
+    if (str.includes(newBrackets[i])) {
+      str = str.replace(newBrackets[i], '');
+      i = -1;
+    }
+  }
+  return !str;
 }
